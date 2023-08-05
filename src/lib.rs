2692da143f0e207055486e25bff7a659aca57962
Dovydas <dovydas@yoshke.org>
2023-08-05 21:53:39 +0300

    implement deserialize

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 use chrono::{DateTime as ChronoDateTime, Utc};
 use url::Url as UrlUrl;
-use yaserde_derive::YaSerialize;
+use yaserde_derive::{YaDeserialize, YaSerialize};
 
+#[derive(Debug, PartialEq)]
 struct DateTime<Tz: chrono::TimeZone>(ChronoDateTime<Tz>);
 
+#[derive(Debug, PartialEq)]
 struct Url(UrlUrl);
 
 /// Sitemap of the website.
-#[derive(YaSerialize)]
+#[derive(Debug, PartialEq, YaSerialize, YaDeserialize)]
 #[yaserde(
     rename = "urlset",
     namespace = "http://www.sitemaps.org/schemas/sitemap/0.9"
@@ -18,15 +20,15 @@ struct Sitemap {
     pages: Vec<Page>,
 }
 
-#[derive(YaSerialize)]
+#[derive(Debug, PartialEq, YaSerialize, YaDeserialize)]
 struct Page {
-    loc: Url,
-    lastmod: DateTime<Utc>,
+    loc: Option<Url>,
+    lastmod: Option<DateTime<Utc>>,
     #[yaserde(prefix = "xhtml")]
     meta: Option<Meta>,
 }
 
-#[derive(YaSerialize)]
+#[derive(Debug, PartialEq, YaSerialize, YaDeserialize)]
 #[yaserde(namespace = "xhtml: http://www.w3.org/1999/xhtml")]
 struct Meta {
     #[yaserde(attribute)]
@@ -71,6 +73,27 @@ impl yaserde::YaSerialize for DateTime<Utc> {
     }
 }
 
+impl yaserde::YaDeserialize for DateTime<Utc> {
+    fn deserialize<R: std::io::Read>(
+        reader: &mut yaserde::de::Deserializer<R>,
+    ) -> Result<Self, String> {
+        loop {
+            match reader.next_event()? {
+                xml::reader::XmlEvent::StartElement { .. } => {}
+                xml::reader::XmlEvent::Characters(ref text_content) => {
+                    return ChronoDateTime::parse_from_rfc3339(text_content)
+                        .map_err(|e| format!("failed to deserialize `{text_content}`: {e}"))
+                        .map(|dt| DateTime(dt.with_timezone(&Utc)));
+                }
+                _ => {
+                    break;
+                }
+            }
+        }
+        Err("Unable to parse".to_string())
+    }
+}
+
 impl yaserde::YaSerialize for Url {
     fn serialize<W>(&self, writer: &mut yaserde::ser::Serializer<W>) -> Result<(), String>
     where
@@ -105,17 +128,45 @@ impl yaserde::YaSerialize for Url {
     }
 }
 
+impl yaserde::YaDeserialize for Url {
+    fn deserialize<R: std::io::Read>(
+        reader: &mut yaserde::de::Deserializer<R>,
+    ) -> Result<Self, String> {
+        loop {
+            match reader.next_event()? {
+                xml::reader::XmlEvent::StartElement { .. } => {}
+                xml::reader::XmlEvent::Characters(ref text_content) => {
+                    return Ok(Url(UrlUrl::parse(text_content).map_err(|e| e.to_string())?));
+                }
+                _ => {
+                    break;
+                }
+            }
+        }
+        Err("Unable to parse".to_string())
+    }
+}
+
 #[test]
 fn test() {
     use pretty_assertions::assert_eq;
 
+    let str_representation = r#"<?xml version="1.0" encoding="utf-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+  <url>
+    <loc>https://example.com/</loc>
+    <lastmod>1970-01-01T00:01:01Z</lastmod>
+    <xhtml:meta name="auto_sitemap_hash" content="1234567890" />
+  </url>
+</urlset>"#;
+
     let sitemap = Sitemap {
         pages: vec![Page {
-            loc: Url(UrlUrl::parse("https://example.com").unwrap()),
-            lastmod: DateTime(ChronoDateTime::<Utc>::from_utc(
+            loc: Some(Url(UrlUrl::parse("https://example.com").unwrap())),
+            lastmod: Some(DateTime(ChronoDateTime::<Utc>::from_utc(
                 chrono::NaiveDateTime::from_timestamp_opt(61, 0).unwrap(),
                 Utc,
-            )),
+            ))),
             meta: Some(Meta {
                 name: "auto_sitemap_hash".into(),
                 content: "1234567890".into(),
@@ -128,16 +179,8 @@ fn test() {
         ..Default::default()
     };
     let serialized = yaserde::ser::to_string_with_config(&sitemap, &yaserde_cfg).unwrap();
+    let deserialized: Sitemap = yaserde::de::from_str(str_representation).unwrap();
 
-    assert_eq!(
-        serialized,
-        r#"<?xml version="1.0" encoding="utf-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-  <url>
-    <loc>https://example.com/</loc>
-    <lastmod>1970-01-01T00:01:01Z</lastmod>
-    <xhtml:meta name="auto_sitemap_hash" content="1234567890" />
-  </url>
-</urlset>"#,
-    );
+    assert_eq!(serialized, str_representation);
+    assert_eq!(deserialized, sitemap);
 }
